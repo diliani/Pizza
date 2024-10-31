baac1a34a9ed1fc39fe4246d839848c8be70fcb2
diliani <132509989+diliani@users.noreply.github.com>
2024-10-31 15:25:27 -0300

    ds

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -4,8 +4,8 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ['Índia','China']
-    const quantidadeDeUsuarios = ['1441000000','1425000000']
+    const nomeDasRedes = ['Índia','China','Estados Unidos','Indonésia','Paquistão','Nigéria','Brasil','Bangladesh','Rússia','Etiópia']
+    const quantidadeDeUsuarios = ['1441000000','1425000000','3410000000','2790000000','2450000000','2290000000','2170000000','1740000000','1430000000','1290000000']
 
     const data = [
         {

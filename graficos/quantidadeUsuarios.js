2d789ac8c157865f7f68a87a38be9bc78c02998c
diliani <132509989+diliani@users.noreply.github.com>
2024-11-04 23:09:31 -0300

    df

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -22,7 +22,7 @@ async function quantidadeUsuariosPorRede() {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Redes sociais com mais usuários no mundo',
+            text: 'Número de idiomas por região:',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),

9d9c6f9f18bc79479d582d0ddf3399fefbba061e
diliani <132509989+diliani@users.noreply.github.com>
2024-11-07 13:46:52 -0300

    rererere

---
+++ graficos/gr-3.js
@@ -0,0 +1,57 @@
+import { getCSS, tickConfig, criarGrafico } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ['Índia','China','Estados Unidos','Indonésia','Paquistão','Nigéria','Brasil','Bangladesh','Rússia','Etiópia']
+    const quantidadeDeUsuarios = ['1441000000','1425000000','341000000','279000000','245000000','229000000','217000000','174000000','143000000','129000000']
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Países Mais Populosos do Mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'países',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'número de habitantes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+
+    }
+
+    criarGrafico(data, layout)
+}
+
+quantidadeUsuariosPorRede()

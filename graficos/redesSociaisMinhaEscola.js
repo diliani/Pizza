48bafb5d48e35711a9258518a0db4aee8bbcb419
diliani <132509989+diliani@users.noreply.github.com>
2024-11-07 00:00:28 -0300

    ds

--- graficos/redesSociaisMinhaEscola.js
+++ graficos/redesSociaisMinhaEscola.js
@@ -37,7 +37,7 @@ function processarDados(dados) {
         paper_bgcolor: getCSS('--bg-color'),
         height: 700,
         title: {
-            text: 'Redes sociais que as pessoas da minha escola mais gostam',
+            text: 'Países Mais Extensos',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),

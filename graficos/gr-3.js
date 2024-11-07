1ad8d9d061986bf6506bca1d4117d27aa3d1e17f
diliani <132509989+diliani@users.noreply.github.com>
2024-11-07 15:03:11 -0300

    aq

--- graficos/gr-3.js
+++ graficos/gr-3.js
@@ -5,7 +5,7 @@ async function quantidadeUsuariosPorRede() {
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = ['Inglês','Mandarim','Hindi','Espanhol','Francês','Árabe','Bengali','Russo','Português','Indonésio']
-    const quantidadeDeUsuarios = ['1268000000','112000000','637000000','538000000','277000000','274000000','265000000','258000000','252000000','199000000']
+    const quantidadeDeUsuarios = ['1268000000','1120000000','637000000','538000000','277000000','274000000','265000000','258000000','252000000','199000000']
 
     const data = [
         {

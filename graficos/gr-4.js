bfb87959209aea6ff2fcf6682a2b450948c95025
diliani <132509989+diliani@users.noreply.github.com>
2024-11-14 13:24:03 -0300

    rerere

---
+++ graficos/gr-4.js
@@ -0,0 +1,58 @@
+import { getCSS, tickConfig, criarGrafico,incluirTexto } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ['Inglês','Mandarim','Hindi','Espanhol','Francês','Árabe','Bengali','Russo','Português','Indonésio']
+    const quantidadeDeUsuarios = ['1268000000','1120000000','637000000','538000000','277000000','274000000','265000000','258000000','252000000','199000000']
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Idiomas Mais Falados do Mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'idiomas',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'número de falantes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+
+    }
+
+    criarGrafico(data, layout)
+    incluirTexto('Essas informações estão de acordo com a fonte, que é o site da Universidade Metodista de São Paulo.')
+}
+
+quantidadeUsuariosPorRede()

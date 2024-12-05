bce4a4c96523906b980bd722e033ee09e60ac51c
diliani <132509989+diliani@users.noreply.github.com>
2024-12-05 13:34:08 -0300

    ds

--- graficos/gr-5.js
+++ graficos/gr-5.js
@@ -4,8 +4,8 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ['1900','1910','30000','41000','52000','51000','1960','1970','1980','1990','2000','2010','2020']
-    const quantidadeDeUsuarios = ['17000','21000','1920','1930','1940','1950','1960','1970','1980','1990','2000','2010','2020']
+    const nomeDasRedes = ['1900','1910','1920','1930','1940']
+    const quantidadeDeUsuarios = ['17000000','21000000','30000000','41000000','52000000']
 
     const data = [
         {
